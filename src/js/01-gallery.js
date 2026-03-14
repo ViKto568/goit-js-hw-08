cdb554ec86a7d764c14e7cc62057665df019ff6d
ViKto568 <ViTiAi568@gmail.com>
2026-03-14 18:14:55 +0200

    Завдання 1
    Фіксація

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+}); 
+
+console.log(galleryItems);
+
