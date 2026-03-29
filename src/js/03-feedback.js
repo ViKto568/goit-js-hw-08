64fee3edf2f403b9a638d1df9ffabfa581f58037
ViKto568 <ViTiAi568@gmail.com>
2026-03-29 22:40:41 +0300

    Завдання 3 - форма зворотного зв'язку
    Локальне сховище форми зворотного зв'язку з ключем "feedback-form-state"

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
 
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    console.log(JSON.parse(savedData));
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  }
+}
+
+window.addEventListener('load', () => {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+});
